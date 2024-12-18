862512f44996f763202d0b9ed46dbdfc3ca81daf
Shibi Ayyanar <shayyanar2002@gmail.com>
2024-12-17 18:51:47 -0700

    Added New Habit Functionality

--- app/screens/MainScreens/Home.tsx
+++ app/screens/MainScreens/Home.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Button, ImageBackground, Modal, TextInput, FlatList } from 'react-native';
+import { BlurView } from "expo-blur";
 import {router} from 'expo-router';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { Tabs } from "expo-router";
 import Icon from "react-native-vector-icons/FontAwesome";
+
+interface Habit {
+  id: number,
+  HabitName: string
+}
 const Home: React.FC = () => {
+  const [isVisible, setVisible] = useState(false);
+  const [HabitN, setHabit] = useState("");
+  const [currHabits, setcurrHabits] = useState<Habit[]>([]);
+  const addNewHabit = () =>{
+    setcurrHabits ((prevHabits) => [ ...prevHabits, {id: prevHabits.length+1, HabitName: HabitN},])
+  };
+  const deleteHabit = (id: number) =>{
+      setcurrHabits((prevHabits) =>prevHabits.filter((habit)=>(habit.id!=id)))
+  }
+  const addHabitButtonHandler = () => {
+    // Call multiple functions here
+    setVisible(false);
+    addNewHabit();
+ 
+  };
+ 
  return (
   
   <View style = {styles.container}>
@@ -14,18 +36,56 @@ const Home: React.FC = () => {
     source={require("../../../assets/images/colorful-block-print-frame-beige-background.jpg")} // Replace with your image URL or local path
         style={styles.backgroundImage}
         resizeMode="cover">
-    <View style = {styles.dashedBox}>
-    <Text>
-      Add a habit!
-    </Text>
-    </View>
-   
+    
+    <FlatList
+        data={currHabits}
+        keyExtractor={(item: Habit) => item.id.toString()}
+        renderItem={({ item }: { item: Habit }) => (
+          <View style={[styles.dashedBox, { backgroundColor: 'white' }]}>
+            <Text >{item.HabitName} {item.id}</Text>
+            <Button title="Delete" onPress={() => deleteHabit(item.id)} />
+          </View>
+          
+        )}
+       // contentContainerStyle={styles.boxContainer}
+      />
     
     
   
-  <TouchableOpacity style={styles.addButton} onPress={() => router.push('/screens/NewHabit')}>
+  <TouchableOpacity style={styles.addButton} onPress={() => setVisible(true)}>
       <Icon name= "plus" size ={20} color = "#fff"></Icon>
     </TouchableOpacity>
+
+    <Modal
+    animationType="fade" // Animation for opening/closing the popup
+    transparent={true} // Allows the modal background to be transparent
+    visible={isVisible} // Controls visibility
+    onRequestClose={() => setVisible(false)} // Android back button
+    
+    >
+      <BlurView intensity={50} style={styles.blurView}>
+      <View style = {styles.CreateHabit}>
+            <Text style = {styles.HabitSelectionText}>Habit Name</Text>
+                  <View style={styles.inputContainer}>
+                      <TextInput style={styles.input}
+                                value={HabitN}
+                                onChangeText={setHabit}
+                      />
+                          </View>
+                          <Text style = {styles.HabitSelectionText}>Enter a password</Text>
+                          <View style={styles.inputContainer}>
+                              <TextInput
+                                  style={styles.input}
+                                  
+                                  
+                                 
+                              />
+                          </View>
+         
+          <Button title="Close" onPress={() => addHabitButtonHandler()}/> 
+      </View>
+      </BlurView>
+    </Modal>
     </ImageBackground>
   </View>
     
@@ -38,7 +98,7 @@ const styles = StyleSheet.create({
   container: {
       flex: 1,
       justifyContent: 'center',
-      paddingHorizontal: 30,
+      
       backgroundColor: '#F1F1F1',
       position: 'relative'
   },
@@ -51,8 +111,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#007bff", // Circle color
     borderRadius: 200,            // Ensures the button is circular
     padding: 15,
-    top: 580,
-    left:310,                 // Padding to make the circle bigger
+    top: 600,
+    left:330,                 // Padding to make the circle bigger
     width: 50,                  // Width of the circle
     height: 50, 
     justifyContent: "center",    // Center the icon horizontally
@@ -62,11 +122,12 @@ const styles = StyleSheet.create({
   dashedBox: {
     borderWidth: 2,
     borderColor: '#007bff',   // Color of the dashed border
-    borderStyle: 'dashed',    // Dashed border style
-    width: 300,
+    borderRadius: 10,
+    width: 350,
     height: 100,
-    top: -250,
-    left: 15,
+    top: -10,
+    left: 0,
+    marginTop:20,
     justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 20
@@ -75,6 +136,43 @@ const styles = StyleSheet.create({
     flex: 1, // Makes the background image fill the screen
     justifyContent: "center", // Centers content vertically
     alignItems: "center",     // Centers content horizontally
-  }
+  },
+  CreateHabit: {
+    width:330,
+    height: 640,
+    top: -10,
+    left: 0,
+    borderRadius: 20,
+    backgroundColor: "white",
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  blurView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.3)", // Adds a darker overlay with transparency
+  },
+  input: {
+    height: 50,
+    borderColor: '#ccc',
+    top:-110,
+    width:250,
+    marginTop: 20,
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    fontSize: 16,
+},
+inputContainer: {
+  marginBottom: 20,
+
+  position: 'relative',
+},
+HabitSelectionText:{
+  top: -100,
+  fontSize: 17
+}
+
 })
 export default Home;
--- app/screens/NewHabit.tsx
+++ app/screens/NewHabit.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import {router} from 'expo-router';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -9,9 +10,17 @@ const NewHabit: React.FC = () => {
  return (
   
   <View style ={styles.container}>
-    <Text style = {styles.welcomeText}>
+    <Text style = {styles.HabitType}>
       New Habit
     </Text>
+    <View style = {styles.inputContainer}>
+      <TextInput style = {styles.input} placeholder="Enter password"/>
+    </View>
+    <TouchableOpacity style = {styles.createHabitButton} onPress={() => router.push('/screens/MainScreens/Home')}>
+      <Text>
+        Create Habit
+      </Text>
+    </TouchableOpacity>
 
   </View>
 
@@ -29,8 +38,26 @@ const styles = StyleSheet.create({
       backgroundColor: '#F1F1F1',
   },
   HabitType: {
-    
+    fontSize: 17,
+    top:-100,
+    color: '#3D3D3D',
+    marginBottom: 20,
   },
+  inputContainer: {
+    marginBottom: 20,
+    
+    position: 'relative',
+},
+input: {
+  height: 40,
+  borderColor: "gray",
+  borderWidth: 1,
+  borderRadius: 5,
+  width: "100%",
+  paddingHorizontal: 10,
+  backgroundColor: "white",
+  position: "relative"
+},
   welcomeText: {
     fontSize: 30,
     height:44,
@@ -40,6 +67,14 @@ const styles = StyleSheet.create({
     textAlign: 'left',
     color: 'blue'
   
+},
+createHabitButton: {
+  backgroundColor: '#2E24B5',
+  paddingVertical: 15,
+  borderRadius: 25,
+  alignItems: 'center',
+  height: 55,
+  top:80
 }
   
 });
